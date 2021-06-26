9e70dd042415719e1d177a561555b36ebf3f5f3b
ppidcard <tvxqlikei@live.cn>
2021-06-26 13:26:20 +1000

    done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime? n
+    n > 1 ?  (
+        for a in 2..(n - 1)
+         if (n % a) == 0
+          return false
+         end
+        end
+        true) : false
+    end
+p prime?(4)
